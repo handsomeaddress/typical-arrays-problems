024dc9df350474d8963e140be85a5cb4f9c2550e
handsomeaddress <yeiehahaha@gmail.com>
2021-02-16 22:24:43 +0300

    init: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(Array.isArray(array) && array.length > 0){
+		let min = array['0'];
+		array.forEach(element => {
+			element < min ? min = element : true;
+		});
+		return min;
+	}else{
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(Array.isArray(array) && array.length > 0){
+		let max = array['0'];
+		array.forEach(element => {
+			element > max ? max = element : true;
+		});
+		return max;
+	}else{
+		return 0;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(Array.isArray(array) && array.length > 0){
+		let i   = 0,
+			avg = 0;
+		array.forEach(element => {
+			avg += element;
+			i++;
+		});
+		return avg/i;
+	}else{
+		return 0;
+	}
 }
